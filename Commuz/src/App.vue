8afe5eddc75d56179e12d18170d13f04d1e155af
fred3105 <66426745+fred3105@users.noreply.github.com>
2023-11-01 19:30:31 +0100

    Starting Layout

--- Commuz/src/App.vue
+++ Commuz/src/App.vue
@@ -1,85 +1,87 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const isSidebarExtended = ref(false);
+
+const toggleSidebar = () => {
+  isSidebarExtended.value = !isSidebarExtended.value;
+};
+
+function extendSidebar() {
+  if (!isSidebarExtended.value) {
+    isSidebarExtended.value = true;
+  }
+}
+
+function retractSidebar() {
+  if (isSidebarExtended.value) {
+    isSidebarExtended.value = false;
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="app">
+    <div class="sidebar" :style="{ width: isSidebarExtended ? '12%' : '4%' }" @mouseover="extendSidebar" @mouseleave="retractSidebar">
+      <div class="logo" @click="toggleSidebar">
+      </div>
+      <RouterLink to="/" v-show="isSidebarExtended">Commuz</RouterLink>
+      <RouterLink to="/Partenaires" v-show="isSidebarExtended">Partenaires</RouterLink>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+
+.sidebar {
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  background: #000000;
+  color: #ffffff;
+  padding: 1rem;
+  transition: width 0.3s ease;
+  z-index: 999;
 }
 
 .logo {
+  height: 50px;
+  background: url('./assets/photo/logo.svg') no-repeat center center;
+  background-size: contain;
+  margin-bottom: 40px;
+}
+
+.sidebar a {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+  margin-bottom: 1rem;
+  text-decoration: none;
   text-align: center;
-  margin-top: 2rem;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 100%;
+  color: #ffffff;
+  transition: opacity 0.3s ease;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.sidebar a:hover {
+  opacity: 0.6;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.content {
+  float: fixed;
+  margin-left: 5%;
+  width: 95%;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
